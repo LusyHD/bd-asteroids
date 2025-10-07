775739fd3bc94b0a2ea7f139bc2bba622a5f7dca
LusyHD <dylanb158@gmail.com>
2025-10-07 11:49:14 -0400

    created player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
@@ -14,12 +15,15 @@ def main():
     clock = pygame.time.Clock()
     dt= 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000 #Set 60fps
